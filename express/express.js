6058c602db7f560e90648c10b83ca50013968f36
Ali Noor <67020631+alinoorisnerd@users.noreply.github.com>
2025-07-19 12:55:30 +0000

    new

--- express/express.js
+++ express/express.js
@@ -1,6 +1,9 @@
 import express from "express";
 const app = express();
 const port = 3000;
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.listen(port, (req, res)=>{
     console.log("Your server has started on port 3000");
@@ -8,6 +11,7 @@ app.listen(port, (req, res)=>{
 
 app.get ("/", (req, res)=>{
     console.log(req.rawHeaders);
+    res.sendFile(__dirname  +  "/index.html")
 });
 
 
