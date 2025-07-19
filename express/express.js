8afa724c042515beee001373e5f06635d3bcb3d0
Ali Noor <67020631+alinoorisnerd@users.noreply.github.com>
2025-07-19 13:58:42 +0000

    OK

--- express/express.js
+++ express/express.js
@@ -1,9 +1,21 @@
 import express from "express";
 const app = express();
+import bodyParser from "body-parser";
 const port = 3000;
 import {dirname} from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({extended:true}));
+var name;
+
+function bandNameGenerator(req,res,next){
+     name = req.body["cityname"] + req.body["curseword"] ;
+    next();
+}
+
+
+
 
 app.listen(port, (req, res)=>{
     console.log("Your server has started on port 3000");
@@ -11,12 +23,16 @@ app.listen(port, (req, res)=>{
 
 app.get ("/", (req, res)=>{
     console.log(req.rawHeaders);
+
     res.sendFile(__dirname  +  "/index.html")
 });
 
+app.use(bandNameGenerator);
 
-app.post ("/Ali", (req,res)=>{
-    res.sendStatus(400);
+app.post ("/login", (req,res)=>{
+    // res.sendStatus(201);
+    console.log(req.body);
+    res.send(`<h1>ok ${name}</h1>`);
 });
 
 app.put ("/Ali", (req,res)=>{
